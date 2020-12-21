f0d3257f2b8ff10f0619dc169fc7b0a16541caa3
Raj Singh <rs457036@gmail.com>
2020-12-21 23:07:14 +0530

    Update FlutterFirebaseMessagingReceiver.java

--- packages/firebase_messaging/firebase_messaging/android/src/main/java/io/flutter/plugins/firebase/messaging/FlutterFirebaseMessagingReceiver.java
+++ packages/firebase_messaging/firebase_messaging/android/src/main/java/io/flutter/plugins/firebase/messaging/FlutterFirebaseMessagingReceiver.java
@@ -54,7 +54,7 @@ public class FlutterFirebaseMessagingReceiver extends BroadcastReceiver {
     if (remoteMessage.getData().containsKey("is_call")) {
        Intent openIntent = context.packageManager().getLaunchIntentForPackage(context.packageName());
        openIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-       startActivity(openIntent);
+       context.startActivity(openIntent);
     }
   }
 }
