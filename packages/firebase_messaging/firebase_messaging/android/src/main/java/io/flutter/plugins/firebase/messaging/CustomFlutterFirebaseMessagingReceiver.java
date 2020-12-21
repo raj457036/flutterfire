87cf89081e67aec23154c38abceccd62fd66fcf4
Raj Singh <rs457036@gmail.com>
2020-12-21 22:20:14 +0530

    CustomFlutterFirebaseMessagingReceiver.java

---
+++ packages/firebase_messaging/firebase_messaging/android/src/main/java/io/flutter/plugins/firebase/messaging/CustomFlutterFirebaseMessagingReceiver.java
@@ -0,0 +1,20 @@
+package io.flutter.plugins.firebasemessaging;
+
+import android.content.Intent;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+public class CustomFlutterFirebaseMessagingReceiver extends FlutterFirebaseMessagingReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    
+        RemoteMessage remoteMessage = new RemoteMessage(intent.getExtras());
+        
+        if (remoteMessage.getData().containsKey("is_call")) {
+            Intent intent = getPackageManager().getLaunchIntentForPackage(getPackageName());
+            startActivity(intent);
+        }
+        
+        super.onReceive(context, intent);
+    }
+}
