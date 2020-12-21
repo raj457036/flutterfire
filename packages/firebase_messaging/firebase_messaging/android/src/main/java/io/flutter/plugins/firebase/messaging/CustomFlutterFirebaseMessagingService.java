9c5ed839e05bc6dff44a157885215f7a628a08c6
Raj Singh <rs457036@gmail.com>
2020-12-21 23:28:17 +0530

    Update CustomFlutterFirebaseMessagingService.java

--- packages/firebase_messaging/firebase_messaging/android/src/main/java/io/flutter/plugins/firebase/messaging/CustomFlutterFirebaseMessagingService.java
+++ packages/firebase_messaging/firebase_messaging/android/src/main/java/io/flutter/plugins/firebase/messaging/CustomFlutterFirebaseMessagingService.java
@@ -3,11 +3,16 @@ package io.flutter.plugins.firebase.messaging;
 import android.content.Intent;
 
 import com.google.firebase.messaging.RemoteMessage;
+import java.io.*; 
+
 
 public class CustomFlutterFirebaseMessagingService extends FlutterFirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+        System.out.print("GfG1"); 
+        System.out.print(remoteMessage.getData());
         if (remoteMessage.getData().containsKey("is_call")) {
+            System.out.print("has key"); 
             Intent intent = getPackageManager().getLaunchIntentForPackage(getPackageName());
             startActivity(intent);
         } else
